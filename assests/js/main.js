c186045e8297e60d2c883a027dfe9285fdbb1515
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-10-28 12:11:55 +0600

    ok

--- assests/js/main.js
+++ assests/js/main.js
@@ -5,3 +5,8 @@ function openMenu(){
 function closeMenu(){
   navBar.style.left = '-100%'
 }
+var latestVid = document.querySelector('.latest_video');
+var latestVidDes = document.querySelector('.latest-vid-des');
+latestVid.src = 'https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fassadikofficial%2Fvideos%2F1561043297729099%2F&show_text=false&width=476&t=0';
+
+latestVidDes.innerHTML = 'সুরা ইয়াসিন | উমর হিশাম আল আরাবি;/n আয়াতঃ ১-১০'
